5ab7fb03aedd334a96d86c129e9eab2593aeb653
JD Huntington <jdhuntington@engineyard.com>
2011-11-11 21:47:33 -0800

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stresster::Application.config.secret_token = '4a4b71dfbd6393e67aa6ddb4279d944be3afa02bb685962ea55efe508889525b549121782887a0e9d481ba935e24efc939f8fb634de044bed872cfdd958ae22c'
